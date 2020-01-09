5ac514796402d83ee54e782bdb6788ba6342aa14
Ian Fowler <Ian@dhcp-1806.guestnet.imsa.edu>
2020-01-09 15:47:27 -0600

    Circle Progress bar

--- src/MainTab/Scout/MatchCell.js
+++ src/MainTab/Scout/MatchCell.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container, Separator, Header, Title, Accordion, ListItem, Content, Footer, Card, CardItem, FooterTab, Button, Left, Right, Body, Badge, H1, H2, H3, Item, Input, Icon} from 'native-base';
 import { FlatList, StyleSheet, View, Text} from 'react-native';
 import PropTypes from 'prop-types';
-
+import { Bar, Pie, Circle, CircleSnail} from 'react-native-progress';
 
 export default class MatchCell extends React.Component {
 
@@ -11,14 +11,15 @@ export default class MatchCell extends React.Component {
         scouts: PropTypes.number.isRequired,
     };
 
-
+    // TODO: FIX HARDCODED 12 SCOUTS
     render () {
         return (
             
             <ListItem>
                 <View style={styles.listItem}>
                     <Text style={styles.match}>{"Match "+this.props.number}</Text>
-                    <Text style={styles.scouts}>{"Scouts: "+this.props.scouts+"/12"}</Text>
+                    <Text style={styles.scouts}>{"Scouts"}</Text>
+                    <Circle progress={this.props.scouts / 12.0} formatText={() => this.props.scouts + "/12"} extStyle={styles.scouts} showsText={true}/>
                 </View>
             </ListItem>
 
@@ -30,15 +31,18 @@ const styles = StyleSheet.create({
   match: {
     color: 'black',
     fontFamily: "Proxima Nova",
+    fontSize: 20,
   },
   scouts: {
     color: 'black',
     fontFamily: "Proxima Nova",
-
+    fontSize: 20,
   },
   listItem: {
-      alignContent: 'space-between',
-      flexDirection: 'row',
-      height: 40,
+    alignContent: 'space-between',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    height: 60,
+    alignItems: 'center',
   }
 });
