ca34f65c4da7bf3ce59e13d5dbde4a9fb21a0987
Dev <dev@singhk.dev>
2020-02-16 17:39:38 -0600

    stop bombarding the user with auth

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ GoogleSignin.configure({
   offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
   hostedDomain: '', // specifies a hosted domain restriction
   loginHint: '@imsa.edu', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
-  forceConsentPrompt: true, // [Android] if you want to show the authorization prompt at each login.
+  forceConsentPrompt: false, // [Android] if you want to show the authorization prompt at each login.
   accountName: '', // [Android] specifies an account name on the device that should be used
   iosClientId: '291863698243-ovppseib28p6usahf60igsp7ia3ovq6l.apps.googleusercontent.com', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
 });
