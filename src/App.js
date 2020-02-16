980e8ab15d887581a49429e6b53f81841ee0721f
Ian Fowler <Ian@ian-macbook-pro.attlocal.net>
2020-02-16 12:58:02 -0600

    Signing in

--- src/App.js
+++ src/App.js
@@ -23,13 +23,14 @@ import {
   View,
   Button,
 } from 'react-native';
+import ajax from './ajax';
 
-
+// Other Web Client ID: 291863698243-t3adrufmitbd3ulgejs8pq255jvvuv9u.apps.googleusercontent.com
 // Web client ID 291863698243-8u79bk1a6odv021fu0km8htvpu6k2uqo.apps.googleusercontent.com
 // ios Client ID: 291863698243-ovppseib28p6usahf60igsp7ia3ovq6l.apps.googleusercontent.com
 GoogleSignin.configure({
   scopes: ['https://www.googleapis.com/auth/userinfo.profile'], // what API you want to access on behalf of the user, default is email and profile
-  webClientId: '291863698243-t3adrufmitbd3ulgejs8pq255jvvuv9u.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+  webClientId: '291863698243-8u79bk1a6odv021fu0km8htvpu6k2uqo.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
   offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
   hostedDomain: '', // specifies a hosted domain restriction
   loginHint: '@imsa.edu', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
@@ -52,7 +53,6 @@ class SignInScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Button title="Sign in!" onPress={this._signInAsync} />
         <GoogleSigninButton
           style={{ width: 192, height: 48 }}
           size={GoogleSigninButton.Size.Wide}
@@ -65,12 +65,8 @@ class SignInScreen extends React.Component {
 
   _googleSignIn = async () => {
     try {
-      console.warn("Dummy");
-      // await GoogleSignin.hasPlayServices();
-      const userInfo = GoogleSignin.signIn();
-      this.setState({ userInfo });
-      console.warn(userInfo);
-      console.log(userInfo);
+      const idToken = await ajax.getIDToken()
+      console.log(idToken);
     } catch (error) {
       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
         // user cancelled the login flow
