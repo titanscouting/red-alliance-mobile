f1288c6ea824925d92fd858c4a1d488b61125f90
Ian Fowler <Ian@ian-macbook-pro.attlocal.net>
2020-02-16 11:22:57 -0600

    SIgnin work

--- src/App.js
+++ src/App.js
@@ -25,6 +25,18 @@ import {
 } from 'react-native';
 
 
+// Web client ID 291863698243-8u79bk1a6odv021fu0km8htvpu6k2uqo.apps.googleusercontent.com
+// ios Client ID: 291863698243-ovppseib28p6usahf60igsp7ia3ovq6l.apps.googleusercontent.com
+GoogleSignin.configure({
+  scopes: ['https://www.googleapis.com/auth/userinfo.profile'], // what API you want to access on behalf of the user, default is email and profile
+  webClientId: '291863698243-t3adrufmitbd3ulgejs8pq255jvvuv9u.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+  offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+  hostedDomain: '', // specifies a hosted domain restriction
+  loginHint: '@imsa.edu', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+  forceConsentPrompt: true, // [Android] if you want to show the authorization prompt at each login.
+  accountName: '', // [Android] specifies an account name on the device that should be used
+  iosClientId: '291863698243-ovppseib28p6usahf60igsp7ia3ovq6l.apps.googleusercontent.com', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+});
 // Implementation of HomeScreen, OtherScreen, SignInScreen, AuthLoadingScreen
 // goes here.
 
@@ -42,11 +54,11 @@ class SignInScreen extends React.Component {
       <View style={styles.container}>
         <Button title="Sign in!" onPress={this._signInAsync} />
         <GoogleSigninButton
-    style={{ width: 192, height: 48 }}
-    size={GoogleSigninButton.Size.Wide}
-    color={GoogleSigninButton.Color.Dark}
-    onPress={this._signInAsync}
-    disabled={this.state.isSigninInProgress} />
+          style={{ width: 192, height: 48 }}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={this._signInAsync}
+          disabled={this.state.isSigninInProgress} />
       </View>
     );
   }
