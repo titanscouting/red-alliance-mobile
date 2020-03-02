f2919769461f9d4f3ebb00c7fd036366b3fc0069
Ian Fowler <Ian@scn-111-125.airstudent.imsa.edu>
2020-03-01 23:25:05 -0600

    Progress

--- src/MainTab/Stats/Tabs/Matches.js
+++ src/MainTab/Stats/Tabs/Matches.js
@@ -4,10 +4,37 @@ import { Form, Container, Header, Title, Accordion, TabHeading, StyleProvider, C
 
 import { FlatList, StyleSheet, ActivityIndicator, RefreshControl, SafeAreaView, View , BackHandler, TouchableWithoutFeedback} from 'react-native';
 import PropTypes from 'prop-types';
-
+import ajax from '../../../ajax';
 
 
 export default class Matches extends React.Component {
+
+    static propTypes = {
+        team: PropTypes.number.isRequired,
+    }
+
+    componentDidMount() {
+        ajax.fetchMatchDataForTeamInCompetition()
+    }
+
+
+    state = {
+        refreshing: false,
+        statsData: null,
+    }
+
+    refreshTeam = async () => {
+
+    }
+
+    onRefresh = async () => {
+        this.setState({refreshing: true});
+        await this.refreshTeam();
+        this.setState({refreshing: false});
+    }
+
+
+
     render() {
       return (
         <Container>
