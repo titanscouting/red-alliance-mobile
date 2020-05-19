af4dfe9c0a26fa5c7a9c6ba655e6c2d37d10ac71
Dev Singh <dev@singhk.dev>
2020-05-18 21:42:45 -0500

    add spacing
    Signed-off-by: Dev Singh <dev@singhk.dev>

--- src/GlobalDefinitions.js
+++ src/GlobalDefinitions.js
@@ -1,27 +1,25 @@
 export default {
-
  "colors":{
-        "blue":"#1F8AE8",
-        "light-blue":"#4B9AFF",
-        "really-dark-blue":"136cb9",
+        "blue": "#1F8AE8",
+        "light-blue": "#4B9AFF",
+        "really-dark-blue": "136cb9",
+        "red": "#CC2232",
+        "light-red": "#E62C41",
+        "really-dark-red": "#ae1e2c",
 
-        "red":"#CC2232",
-        "light-red":"#E62C41",
-        "really-dark-red":"#ae1e2c",
-
-        "light-red-tint":"#f08f9d",
+        "light-red-tint": "#f08f9d",
     },
     "brand":{
-        "primary":"red",
-        "primary-light":"light-red",
-        "primary-dark":"really-dark-red",
-        "secondary":"blue",
-        "secondary-light":"light-blue",
-        "secondary-dark":"really-dark-blue",
-        "tint":"light-red-tint",
+        "primary": "red",
+        "primary-light": "light-red",
+        "primary-dark": "really-dark-red",
+        "secondary": "blue",
+        "secondary-light": "light-blue",
+        "secondary-dark": "really-dark-blue",
+        "tint": "light-red-tint",
     },
     "data":{
-        "competition":"2020ilch",
+        "competition": "2020ilch",
         "version": "3.4.1"
     }
 };
