df6ddda0e299cd378683816cf6af34816600b2d7
Dev Singh <dev@devksingh.com>
2020-07-16 19:09:33 -0500

    fix syntax errors

--- src/MainTab/Scout/Matches.js
+++ src/MainTab/Scout/Matches.js
@@ -1,142 +1,163 @@
 import React from 'react';
-import { Form, Container, Header, Title, Accordion, StyleProvider, Content, Footer, Card, CardItem, FooterTab, Button, Left, Right, Body, Text, Badge, H1, H2, H3, Item, Input, Icon} from 'native-base';
+import {StyleProvider} from 'native-base';
 import getTheme from '../../../native-base-theme/components';
 import material from '../../../native-base-theme/variables/material';
 
-import { FlatList, ActivityIndicator, View} from 'react-native';
-import PropTypes from 'prop-types';
-import MatchCell from './MatchList/MatchCell';
 import MatchList from './MatchList/MatchList';
 import TeamList from './TeamsList/TeamList';
-import ajax from '../../ajax'
-import { StackActions } from 'react-navigation';
-import GLOBAL from '../../GlobalDefinitions'
-import Eval from './Evaluation/Eval'
+import ajax from '../../ajax';
+import GLOBAL from '../../GlobalDefinitions';
+import Eval from './Evaluation/Eval';
 
 export default class Matches extends React.Component {
+  _isMounted = false;
+  state = {
+    matches: [],
+    currentMatchNumber: null,
 
-    _isMounted = false;
-    state = {
-        matches: [],
-        currentMatchNumber: null,
+    teams: null,
+    currentTeamNumber: null,
+    isBlue: null,
 
-        teams: null,
-        currentTeamNumber: null,
-        isBlue: null,
+    configuration: {},
+  };
 
-        configuration: {}
-    }
+  componentDidMount() {
+    this._isMounted = true;
+    this.refreshMatches();
+    this.pullConfiguration();
+  }
 
-    componentDidMount() {
-        this._isMounted = true;
-        this.refreshMatches();
-        this.pullConfiguration();
-    }
+  refreshMatches = async () => {
+    const matches = await ajax.fetchMatches(GLOBAL.data.competition);
+    this.state.matches = matches;
+    this.forceUpdate();
+  };
 
-    refreshMatches = async () => {
-        const matches = await ajax.fetchMatches(GLOBAL.data.competition);
-        this.state.matches = matches
-        this.forceUpdate()
-    }
+  currentMatch = () => {
+    return this.matches.find(match => match.key === this.currentMatchNumber);
+  };
 
+  async componentWillUnmount() {
+    this._isMounted = false;
+  }
 
-    currentMatch = () => {
-        return this.matches.find((match) => match.key === this.currentMatchNumber);
-    }
+  setCurrentMatch = number => {
+    this.state.currentMatchNumber = number;
+    this.state.teams = [];
+    this.forceUpdate();
+    this.refreshTeams();
+  };
 
-    async componentWillUnmount() {
-        this._isMounted = false;
-    }
+  unsetCurrentMatch = () => {
+    this.state.currentMatchNumber = null;
+    this.state.teams = null;
+    this.forceUpdate();
+  };
 
-    setCurrentMatch = (number) => {
-        this.state.currentMatchNumber = number;
-        this.state.teams = []
-        this.forceUpdate()
+  getTeams = async () => {
+    const teams = await ajax.fetchTeamsForMatch(
+      GLOBAL.data.competition,
+      this.state.currentMatchNumber,
+    );
+    return teams;
+  };
+  refreshTeams = async () => {
+    let teams = await this.getTeams();
+    this.state.teams = teams;
+    this.forceUpdate();
+  };
+
+  setCurrentTeam = async (teamNumber, isBlue) => {
+    let teams = await this.getTeams();
+    for (var team in teams) {
+      let number = teams[team].teamNumber;
+      if (number === teamNumber && teams[team].scouterDescription != null) {
         this.refreshTeams();
+        return;
+      }
     }
+    ajax.addScouterToMatch(teamNumber, this.state.currentMatchNumber);
+    this.state.currentTeamNumber = teamNumber;
+    this.state.isBlue = isBlue;
+    this.forceUpdate();
+    this.pullConfiguration();
+  };
+  unsetCurrentTeam = () => {
+    this.state.currentTeamNumber = null;
+    this.forceUpdate();
+  };
 
-    unsetCurrentMatch = () => {
-        this.state.currentMatchNumber = null;
-        this.state.teams = null;
-        this.forceUpdate()
+  pullConfiguration = async () => {
+    if (
+      !this.state.configuration ||
+      Object.keys(this.state.configuration).length === 0
+    ) {
+      const config = await ajax.fetchMatchConfig();
+      this.setState({configuration: config});
     }
+  };
 
-    getTeams = async () => {
-        const teams = await ajax.fetchTeamsForMatch(GLOBAL.data.competition, this.state.currentMatchNumber);
-        return teams;
-    }
-    refreshTeams = async () => {
-        let teams = await this.getTeams();
-        this.state.teams = teams;        
-        this.forceUpdate()
-    }
+  popEval = () => {
+    this.state.currentTeamNumber = null;
+    this.forceUpdate();
+  };
 
-    setCurrentTeam = async (teamNumber, isBlue) => {
-        let teams = await this.getTeams();
-        for (var team in teams) {
-            let number = teams[team].teamNumber;
-            if (number === teamNumber && teams[team].scouterDescription != null) {
-                this.refreshTeams();
-                return;
-            }
-        }
-        ajax.addScouterToMatch(teamNumber, this.state.currentMatchNumber);
-        this.state.currentTeamNumber = teamNumber;
-        this.state.isBlue = isBlue;
-        this.forceUpdate();
-        this.pullConfiguration();
-    }
-    unsetCurrentTeam = () => {
-        this.state.currentTeamNumber = null;
-        this.forceUpdate()
-    }
-    
-    pullConfiguration = async () => {
-        if (!this.state.configuration || Object.keys(this.state.configuration).length === 0) {
-            const config = await ajax.fetchMatchConfig();
-            this.setState({configuration: config})
-        } 
-    }
+  saveScouting = vals => {
+    ajax.submitMatchData(
+      GLOBAL.data.competition,
+      this.state.currentTeamNumber,
+      this.state.currentMatchNumber,
+      vals,
+    );
+    this.setState({
+      currentMatchNumber: null,
+      teams: null,
+      currentTeamNumber: null,
+      isBlue: null,
+    });
+    this.forceUpdate();
+  };
 
-    popEval = () => {
-        this.state.currentTeamNumber = null;
-        this.forceUpdate();
+  render() {
+    if (this.state.currentTeamNumber != null) {
+      return (
+        <StyleProvider style={getTheme(material)}>
+          <Eval
+            configuration={this.state.configuration}
+            onBack={this.popEval}
+            onSave={this.saveScouting}
+            matchNumber={this.state.currentMatchNumber}
+            teamNumber={this.state.currentTeamNumber}
+            isBlue={this.state.isBlue}
+          />
+        </StyleProvider>
+      );
+    } else if (
+      this.state.currentMatchNumber != null &&
+      this.state.teams != null
+    ) {
+      return (
+        <StyleProvider style={getTheme(material)}>
+          <TeamList
+            teams={this.state.teams}
+            refreshTeams={this.refreshTeams}
+            matchNumber={this.state.currentMatchNumber}
+            onBack={this.unsetCurrentMatch}
+            onItemPress={this.setCurrentTeam}
+          />
+        </StyleProvider>
+      );
+    } else {
+      return (
+        <StyleProvider style={getTheme(material)}>
+          <MatchList
+            matches={this.state.matches}
+            onItemPress={this.setCurrentMatch}
+            refreshMatches={this.refreshMatches}
+          />
+        </StyleProvider>
+      );
     }
-
-    saveScouting = (vals) => {
-        ajax.submitMatchData(GLOBAL.data.competition, this.state.currentTeamNumber, this.state.currentMatchNumber, vals);
-        this.setState({
-            currentMatchNumber: null,
-            teams: null,
-            currentTeamNumber: null,
-            isBlue: null,
-        })
-        this.forceUpdate();
-    }
-
-    render () {
-        if (this.state.currentTeamNumber != null) {
-            return (
-                <StyleProvider style={getTheme(material)}>
-                    <Eval configuration={this.state.configuration} onBack={this.popEval} onSave={this.saveScouting} matchNumber={this.state.currentMatchNumber} teamNumber={this.state.currentTeamNumber} isBlue={this.state.isBlue}/>
-                </StyleProvider>
-                );
-        }
-        else if (this.state.currentMatchNumber != null && this.state.teams != null) {
-            return (
-                <StyleProvider style={getTheme(material)}>
-                    <TeamList teams={this.state.teams} refreshTeams={this.refreshTeams} matchNumber={this.state.currentMatchNumber} onBack={this.unsetCurrentMatch} onItemPress={this.setCurrentTeam}/>
-                </StyleProvider>
-                );
-        }
-        else {
-            return (
-                <StyleProvider style={getTheme(material)}>
-                    <MatchList matches = {this.state.matches} onItemPress={this.setCurrentMatch} refreshMatches={this.refreshMatches}/>
-                </StyleProvider>
-            );
-        }
-
-    }
-    
+  }
 }
--- src/MainTab/TabControl.js
+++ src/MainTab/TabControl.js
@@ -1,59 +1,66 @@
-import React, { Component } from "react";
-import Matches from "./Scout/Matches"
-import Strategies from "./Strategies/Strategies"
-import Stats from "./Stats/Stats"
-import Options from "./Options/Options"
+import React from 'react';
+import Matches from './Scout/Matches';
+import Strategies from './Strategies/Strategies';
+import Stats from './Stats/Stats';
+import Options from './Options/Options';
 
+import {createAppContainer} from 'react-navigation';
 
-import { createAppContainer } from 'react-navigation';
-
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-import { StyleProvider, Button, Text, Icon, Footer, FooterTab,Content} from "native-base";
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {StyleProvider, Button, Icon, Footer, FooterTab} from 'native-base';
 import getTheme from '../../native-base-theme/components';
 import material from '../../native-base-theme/variables/material';
 
-import Container from "../../native-base-theme/components/Container";
-
 const TabControl = createBottomTabNavigator(
   {
-    Teams: { screen: Matches },
-    Stats: { screen: Stats},
-    Strat: { screen: Strategies},
-    Options: {screen: Options}
-  }, {
-    tabBarPosition: "bottom",
+    Teams: {screen: Matches},
+    Stats: {screen: Stats},
+    Strat: {screen: Strategies},
+    Options: {screen: Options},
+  },
+  {
+    tabBarPosition: 'bottom',
 
+    defaultNavigationOptions: ({navigation}) => ({
+      tabBarComponent: () => {
+        const {routeName} = navigation.state;
+        return (
+          <StyleProvider style={getTheme(material)}>
+            <Footer>
+              <FooterTab>
+                <Button
+                  vertical
+                  active={routeName === 'Teams'}
+                  onPress={() => navigation.navigate('Teams')}>
+                  <Icon name="clipboard" />
+                </Button>
 
-    defaultNavigationOptions: ({ navigation }) => ({
-        tabBarComponent: () => {
-            const { routeName } = navigation.state;
-            return (
-                <StyleProvider style={getTheme(material)}>
-                    <Footer>
-                        <FooterTab>
-                            <Button vertical active={routeName === 'Teams'} onPress={() => navigation.navigate("Teams")}>
-                                <Icon name="clipboard" />
-                            </Button>
-                            
-                            <Button vertical active={routeName === 'Stats'} onPress={() => navigation.navigate("Stats")}>
-                                <Icon name="md-stats" />
-                            </Button>
-                            
-                            <Button vertical active={routeName === 'Strat'} onPress={() => navigation.navigate("Strat")}>
-                                <Icon name="git-pull-request" />
-                            </Button>
-                            <Button vertical active={routeName === 'Options'} onPress={() => navigation.navigate("Options")}>
-                                <Icon name="ios-cog" />
-                            </Button>
-                        </FooterTab>
-                    </Footer>
-                </StyleProvider>
-              );
-        }
+                <Button
+                  vertical
+                  active={routeName === 'Stats'}
+                  onPress={() => navigation.navigate('Stats')}>
+                  <Icon name="md-stats" />
+                </Button>
+
+                <Button
+                  vertical
+                  active={routeName === 'Strat'}
+                  onPress={() => navigation.navigate('Strat')}>
+                  <Icon name="git-pull-request" />
+                </Button>
+                <Button
+                  vertical
+                  active={routeName === 'Options'}
+                  onPress={() => navigation.navigate('Options')}>
+                  <Icon name="ios-cog" />
+                </Button>
+              </FooterTab>
+            </Footer>
+          </StyleProvider>
+        );
+      },
     }),
-
-  }
+  },
 );
 
 export default createAppContainer(TabControl);
