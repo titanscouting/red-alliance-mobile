bbadf9cf2a119047cc9888f0eba528b27b29efae
Dev Singh <dev@devksingh.com>
2020-07-18 18:53:07 -0500

    Revert "memes"
    This reverts commit 51cb706024e98aeebdbdcaf62cebbabb2b90e9e3.

--- android/app/src/main/java/com/redalliance/MainApplication.java
+++ android/app/src/main/java/com/redalliance/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import im.shimo.react.prompt.RNPromptPackage;
 import io.xogus.reactnative.versioncheck.RNVersionCheckPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
