cf244a93e343081a7e7067fc15e8094fd44fa7a6
Dev Singh <dsingh@imsa.edu>
2022-01-10 18:45:22 -0600

    fix reanimated for android builds

--- android/app/src/main/java/com/redalliance/MainApplication.java
+++ android/app/src/main/java/com/redalliance/MainApplication.java
@@ -16,6 +16,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.facebook.react.bridge.JSIModulePackage;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 public class MainApplication extends MultiDexApplication implements ReactApplication {
 
@@ -34,6 +36,8 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
           // packages.add(new MyReactNativePackage());
           return packages;
         }
+
+        @Override protected JSIModulePackage getJSIModulePackage() { return new ReanimatedJSIModulePackage(); }
 
         @Override
         protected String getJSMainModuleName() {
