4520c7f66999b17d03e114323555a979aa29bb73
Dev Singh <dsingh@imsa.edu>
2022-04-02 23:13:32 -0500

    remove background notification empty on android

--- index.js
+++ index.js
@@ -2,13 +2,8 @@
  * @format
  */
 
-import {AppRegistry, Platform} from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
-import messaging from '@react-native-firebase/messaging';
-
-if (Platform.OS === 'android') {
-  messaging().setBackgroundMessageHandler(() => {});
-}
 
 AppRegistry.registerComponent(appName, () => App);
