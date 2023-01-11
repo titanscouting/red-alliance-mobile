7db1c26203475a37d58634888faf371c896c8d14
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-01-11 11:24:55 -0600

    implement grid UI

--- src/MainTab/Scout/Evaluation/Table.js
+++ src/MainTab/Scout/Evaluation/Table.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import {Text} from 'react-native';
+import { CheckBox } from 'native-base';
+import { Col, Row, Grid } from 'react-native-easy-grid'
 
 export default class OurTable extends React.Component {
   static propTypes = {
@@ -8,6 +10,16 @@ export default class OurTable extends React.Component {
   };
 
   render() {
-    return <Text>{'Table implemented'}</Text>;
+    return <Grid>
+      {[...Array(9)].map((_, col) => (
+        <Col>
+          {[...Array(3)].map((_, row) => (
+            <Row style={{aspectRatio: 1}}>
+              <CheckBox style={{width: '100%', height: '100%', flex: 1, alignItems: 'center', justifyContent: 'center'}} color={col / 3 % 2 < 1 ? "red" : "gray"} />
+            </Row>
+          ))}
+        </Col>
+      ))}
+    </Grid>;
   }
 }
